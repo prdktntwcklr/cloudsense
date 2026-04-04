43cc7068b736f2405d5415fcb72bb886a0efcaa4
prdktntwcklr <61001903+prdktntwcklr@users.noreply.github.com>
2026-04-04 06:34:40 +0000

    chore: cleanup sensor driver

--- firmware/drivers/sensor/simulated_sensor/simulated_sensor.c
+++ firmware/drivers/sensor/simulated_sensor/simulated_sensor.c
@@ -2,37 +2,48 @@
 
 #include <zephyr/device.h>
 #include <zephyr/drivers/sensor.h>
+#include <zephyr/sys/__assert.h>
+
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(SIMULATED_SENSOR, CONFIG_SENSOR_LOG_LEVEL);
 
 struct simulated_sensor_data {
     int16_t temp;
 };
 
-static int simulated_sample_fetch(const struct device *dev, enum sensor_channel chan) {
+static int simulated_sensor_sample_fetch(const struct device *dev, enum sensor_channel chan) {
+    __ASSERT_NO_MSG(chan == SENSOR_CHAN_ALL);
+
     struct simulated_sensor_data *data = dev->data;
     data->temp = 25; // Simulate a constant 25°C
+
     return 0;
 }
 
-static int simulated_channel_get(const struct device *dev, enum sensor_channel chan, struct sensor_value *val) {
+static int simulated_sensor_channel_get(const struct device *dev, enum sensor_channel chan, struct sensor_value *val) {
+    __ASSERT_NO_MSG(chan == SENSOR_CHAN_AMBIENT_TEMP);
+    
     struct simulated_sensor_data *data = dev->data;
     val->val1 = data->temp;
     val->val2 = 0;
+
     return 0;
 }
 
-static int simulated_init(const struct device *dev) {
+static int simulated_sensor_init(const struct device *dev) {
     return 0;
 }
 
 static const struct sensor_driver_api simulated_sensor_api = {
-    .sample_fetch = simulated_sample_fetch,
-    .channel_get = simulated_channel_get,
+    .sample_fetch = simulated_sensor_sample_fetch,
+    .channel_get = simulated_sensor_channel_get,
 };
 
 #define SIMULATED_SENSOR_CREATE_INST(inst) \
     static struct simulated_sensor_data simulated_sensor_drv_##inst; \
     DEVICE_DT_INST_DEFINE(inst, \
-                          simulated_init, \
+                          simulated_sensor_init, \
                           NULL, \
                           &simulated_sensor_drv_##inst, \
                           NULL, \
